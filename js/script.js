432b899a017ee2e5884f759456cfab7817dfe7ac
whalecake <ingrid.ang@codenation.org>
2020-01-11 05:59:02 +0000

    Finished project

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let artists = ['James & Kyndal','Destiny & Katie','Hello Sunday & Lauren'];
 let songLength = ['x','y','z'];
 let image = ['https://www.billboard.com/files/styles/article_main_image/public/media/maren-morris-2019-nyc-billboard-1548.jpg',
     'https://imgs.smoothradio.com/images/65221?crop=16_9&width=660&relax=1&signature=rWlcsQiX1cPnOuGNnLL2uPD2Mno=',
-    'z'
+    'https://www.rollingstone.com/wp-content/uploads/2020/01/10323507bj.jpg?resize=900,600&w=450'
 ];
 let songLink = [
     'https://www.youtube.com/watch?v=Ly5edoTyXqg',
@@ -13,23 +13,23 @@ let songLink = [
 let songInput, artistInput, lengthInput, imageInput, linkInput;
 
 songs.forEach(function(title){
-    $(".songList").append('<p>'+title+'</p>');
+    $(".songList").append('<div class="appended text-add">'+title+'</div>');
 });
 
 artists.forEach(function(artist){
-    $(".artistList").append('<p>'+artist+'</p>');
+    $(".artistList").append('<div class="appended text-add">'+artist+'</div>');
 });
 
 songLength.forEach(function(length){
-    $(".lengthList").append('<p>'+length+'</p>');
+    $(".lengthList").append('<div class="appended text-add">'+length+'</div>');
 });
 
 image.forEach(function(image){
-    $(".imageList").append('<img src="'+image+'">');
+    $(".imageList").append('<div class="appended">'+'<img src="'+image+'">'+'</div>');
 });
 
 songLink.forEach(function(link){
-    $(".linkList").append('<p>'+'<a href='+link+'>'+"Listen"+'</a>'+'</p>');
+    $(".linkList").append('<div class="appended text-add">'+'<a href='+link+'>'+"Listen"+'</a>'+'</div>');
 });
 
 $("#submit").click (function() {
@@ -37,18 +37,18 @@ $("#submit").click (function() {
    
     //push all input vals
     songs.push($("#songInput").val());
-    $(".songList").append('<p>'+songs[songs.length-1]+'</p>');
+    $(".songList").append('<div class="appended text-add">'+songs[songs.length-1]+'</div>');
 
     artists.push($("#artistInput").val());
-    $(".artistList").append('<p>'+artists[artists.length-1]+'</p>');
+    $(".artistList").append('<div class="appended text-add">'+artists[artists.length-1]+'</div>');
 
     songLength.push($("#lengthInput").val());
-    $(".lengthList").append('<p>'+songLength[songLength.length-1]+'</p>');
+    $(".lengthList").append('<div class="appended text-add">'+songLength[songLength.length-1]+'</div>');
 
     image.push($("#imageInput").val());
-    $(".imageList").append('<p>'+image[image.length-1]+'</p>');
+    $(".imageList").append('<div class="appended">'+'<img src="'+image[image.length-1]+'">'+'</div>');
 
     songLink.push($("#linkInput").val());
-    $(".linkList").append('<p>'+songLink[songLink.length-1]+'</p>');
+    $(".linkList").append('<div class="appended text-add">'+'<a href="'+songLink[songLink.length-1]+'">'+'Listen'+'</a>'+'</div>');
 
 });
